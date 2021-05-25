add133bad3dce514ac7afcff36f5ce9b9042b587
Diego-Buitrago <die.ar.90@gmail.com>
2021-05-24 22:45:17 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {createRef, useState} from 'react'
+import {Modal, Button, Form, Input} from 'antd'
 
 function App() {
+
+  const [modal, setModal] = useState(false)
+  const [futbolista,  setFutbolisata] = useState({
+    nombre: '',
+    edad: '',
+    posicion,
+    club
+  })
+
+  const abrirModal = () => {
+    setModal(true);
+  }
+
+  const cerrarModal = () => {
+    setModal(false);
+  }
+
+  const accion = () => {
+    alert('Se presiono el ok del modal');
+    cerrarModal();
+  }
+
+  const handerleChange = (e) =>{
+    const {name, value} = e.targe;
+    setFutbolisata({...futbolista,
+    [name]: value});
+  }
+
+  const layout = {
+    labelCol:{
+      span: 8,
+    },
+    wrapperCol: {
+      span: 5,
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button onClick={abrirModal} type="primary">Abrir modal</Button> 
+      <Modal
+        title="Futbolistas"
+        visible={modal}
+        onCansel={cerrarModal}
+        onOk={accion}
+        centered
+        footer={[
+          <Button onClick={cerrarModal}>Cancelar</Button>,
+          <Button onClick={accion}>Enviar</Button>
+        ]}
+      >
+        <Form {...layout}>
+          <Item label="nombre">
+            <Input onChange={handerleChange} name="nombre"></Input>
+          </Item>
+
+          <Item label="edad">
+            <Input onChange={handerleChange} name="edad"></Input>
+          </Item> 
+
+          <Item label="posicion">
+            <Input onChange={handerleChange} name="posicion"></Input>
+          </Item> 
+
+          <Item label="club">
+            <Input onChange={handerleChange} name="club"></Input>
+          </Item> 
+        </Form>
+      </Modal>
     </div>
   );
 }
